7f994c0ad07c36d30ae8dfbcb4865df61983de1a
Minjoo Jang <miinjoojang@gmail.com>
2022-04-21 03:36:52 +0900

    [FIX] 서포터 프로젝트신고내역 목록조회용 DTO 생성 및 수정
    누락된 커밋사항이 있어 추가합니다.

--- src/main/java/com/greedy/dduckleaf/projectreport/find/dto/ReportCategoryDTO.java
+++ src/main/java/com/greedy/dduckleaf/projectreport/find/dto/ReportCategoryDTO.java
@@ -1,2 +1,14 @@
-package com.greedy.dduckleaf.projectreport.find.dto;public class ReportCategoryDTO {
+package com.greedy.dduckleaf.projectreport.find.dto;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class ReportCategoryDTO {
+
+    private int reportCategoryNo;
+    private String reportCategoryName;
+    private String reportCategoryContent;
 }
