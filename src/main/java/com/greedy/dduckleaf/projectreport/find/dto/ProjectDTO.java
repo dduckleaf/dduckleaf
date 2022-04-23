054c8c27095afb174d03d34ee9cd0f7141dc38dd
Minjoo Jang <miinjoojang@gmail.com>
2022-04-24 00:58:38 +0900

    [FEAT] 엔티티, DTO 클래스 저장경로 수정

---
+++ src/main/java/com/greedy/dduckleaf/projectreport/find/dto/ProjectDTO.java
@@ -0,0 +1,13 @@
+package com.greedy.dduckleaf.projectreport.find.dto;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class ProjectDTO {
+
+    private int projectNo;
+    private String projectName;
+}
