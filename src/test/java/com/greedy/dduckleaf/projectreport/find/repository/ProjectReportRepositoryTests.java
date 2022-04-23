c6e7de012a99d44d374d52e34d760548c87ae153
Minjoo Jang <miinjoojang@gmail.com>
2022-04-24 01:02:07 +0900

    [TEST] 프로젝트 전체목록 조회용 컨트롤러, 서비스, 레파지토리 클래스의 테스트코드 작성
    컨트롤러 테스트코드는 수정보완이 좀더 필요합니다. 완성되는대로 커밋하도록 하겠습니다

--- src/test/java/com/greedy/dduckleaf/projectreport/find/repository/ProjectReportRepositoryTests.java
+++ src/test/java/com/greedy/dduckleaf/projectreport/find/repository/ProjectReportRepositoryTests.java
@@ -1,22 +1,24 @@
 package com.greedy.dduckleaf.projectreport.find.repository;
 
 import com.greedy.dduckleaf.config.*;
-import com.greedy.dduckleaf.projectreport.find.entity.Member;
 import com.greedy.dduckleaf.projectreport.find.entity.ProjectReport;
-import com.greedy.dduckleaf.projectreport.detail.repository.ProjectReportRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ContextConfiguration;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ContextConfiguration(classes = {
@@ -32,7 +34,7 @@ public class ProjectReportRepositoryTests {
     private EntityManager entityManager;
 
     @Autowired
-    private MemberForProjectReportRepository repository;
+    private ProjectReportRepository repository;
 
     @Test
     public void initTest() {
@@ -41,24 +43,33 @@ public class ProjectReportRepositoryTests {
     }
 
     @Test
-    @DisplayName("서포터 정보로 프로젝트신고내역 목록 조회")
-    @Transactional
-    public void test3() {
+    @DisplayName("모든 프로젝트신고내역 목록조회 테스트")
+    public void findAll_test() {
+        //given
+        Pageable pageable = PageRequest.of(0,
+                10,
+                Sort.by("projectReportNo").descending());
 
-        int memberNo = 3;
+        //when
+        Page<ProjectReport> projectReportList = repository.findAll(pageable);
 
-        Member member = repository.findById(memberNo).get();
-        assertNotNull(member);
-        System.out.println("member = " + member);
-
-        List<ProjectReport> projectReportList = member.getProjectReportList();
+        //then
         assertNotNull(projectReportList);
-        projectReportList.forEach( projectReport -> {
-            System.out.println("projectReport = " + projectReport);
-            System.out.println("projectReport.getProject().getProjectName() = " + projectReport.getProject().getProjectName());
-            System.out.println("projectReport.getReportCategory().getReportCategoryName() = " + projectReport.getReportCategory().getReportCategoryName());
-        });
+        projectReportList.forEach(System.out::println);
 
-        System.out.println("projectReportList.size() =  " + projectReportList.size());
+    }
+
+    /*=========================================너는 사용하지 말아봐봐*/
+    @Test
+    @DisplayName("모든 프로젝트신고내역 목록조회")
+    public void findAllProjectReport_test() {
+        //given
+
+        //when
+        List<Object> projectReportList = repository.findAllProjectReport();
+
+        //then
+        assertNotNull(projectReportList);
+        projectReportList.forEach(System.out::println);
     }
 }
