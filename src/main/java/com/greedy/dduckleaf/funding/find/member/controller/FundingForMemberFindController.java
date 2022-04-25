715465b51f27ae142dcd35847460d3bfe136f57e
DESKTOP-ONN8F4O\h99ww <h99www@gmail.com>
2022-04-26 00:23:55 +0900

    [FEAT] 펀딩 조회 데이터 연동 작성

--- src/main/java/com/greedy/dduckleaf/funding/find/member/controller/FundingForMemberFindController.java
+++ src/main/java/com/greedy/dduckleaf/funding/find/member/controller/FundingForMemberFindController.java
@@ -3,6 +3,7 @@ package com.greedy.dduckleaf.funding.find.member.controller;
 import com.greedy.dduckleaf.authentication.model.dto.CustomUser;
 import com.greedy.dduckleaf.funding.dto.FundingDTO;
 import com.greedy.dduckleaf.funding.find.member.service.FundingServiceForFind;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,25 +15,37 @@ import java.util.List;
 /**
  * <pre>
  * Class : FundingForMemberFindController
- * Comment : 회원의 펀딩 조회를 담당하는 핸들러메소드를 정의해놓은 클래스 입니다.
+ * Comment : 회원의 펀딩 조회를 담당하는 핸들러메소드를 정의해놓은 클래스
  *
  * History
- * 2022-04-25 h99ww
+ * 2022-04-25 (홍성원) 처음 작성 / sendMemberFundingListPage / findFundingListByMemberNo 메소드 작성
  * </pre>
- *
- * @author h99ww
  * @version 1.0.0
+ * @author (홍성원)
  */
 @Controller
 @RequestMapping("/funding/find")
 public class FundingForMemberFindController {
 
+    /* 회원의 펀딩조희 프로세스의 로직을 처리하는 서비스 계층의 인스턴스 변수를 선언한다. */
     private final FundingServiceForFind service;
 
+    @Autowired
     public FundingForMemberFindController(FundingServiceForFind service) {
         this.service = service;
     }
 
+
+
+
+
+    /**
+     * sendMemberFundingListPage :
+     * @param
+     * @return
+     *
+     * @author 홍성원
+     */
     @GetMapping("/detail/member")
     public ModelAndView sendMemberFundingListPage(ModelAndView mv) {
 
@@ -53,8 +66,8 @@ public class FundingForMemberFindController {
         int memberNo = user.getMemberNo();
         List<FundingDTO> fundingList = service.findFundingByMemberNo(memberNo);
         fundingList.forEach(fundingDTO -> {
-            System.out.print("FundingInfoNo() = " + fundingDTO.getFundingInfoNo());
-            System.out.println(" > getProgressStatus() = " + fundingDTO.getProject().getProgressStatus());
+            System.out.println("fundingDTO = " + fundingDTO);
+            System.out.println();
         });
         mv.addObject("fundingList", fundingList);
         mv.setViewName("/funding/find/supporter/fundinglist");
