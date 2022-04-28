0cfcef02d52be8d239cc32232807f7f2e405a1bb
DESKTOP-RR7C6JL\Administrator <tkdjawlwhs@gmail.com>
2022-04-28 23:17:09 +0900

    [FEAT] 마이펀딩 마이펀딩 조회
    펀딩 횟수, 모의 펀딩 횟수, 티켓 갯수를 조회하는 마이펀딩 조회 기능을 구현하였습니다.

---
+++ src/main/java/com/greedy/dduckleaf/myfunding/dto/MyFundingDTO.java
@@ -0,0 +1,15 @@
+package com.greedy.dduckleaf.myfunding.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class MyFundingDTO {
+
+    private int fundingTimes;
+    private int mockFundingTimes;
+    private int ticketAmount;
+}
