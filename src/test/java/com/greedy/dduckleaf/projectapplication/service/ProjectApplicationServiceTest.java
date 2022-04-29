aa1a486a6a13c5339d52fee2cc00f9a0d9e5b98c
LAPTOP-CBFQ98L2\hwiri <hwirim7907@gmail.com>
2022-04-30 03:30:25 +0900

    [TEST] 프로젝트 신청 반환정책 작성, 수정 테스트 코드

--- src/test/java/com/greedy/dduckleaf/projectapplication/service/ProjectApplicationServiceTest.java
+++ src/test/java/com/greedy/dduckleaf/projectapplication/service/ProjectApplicationServiceTest.java
@@ -179,7 +179,6 @@ class ProjectApplicationServiceTest {
         //when
         ProjectBasicInfoDTO updateStory = projectApplicationService.findProjectBasicInfoByProjectNo(projectNo);
         updateStory.setProjectPromotionAgreementStatus("Y");
-//        updateStory.setRewardAgreementDate("0000-00-00");
 
         //then
         assertDoesNotThrow(() -> projectApplicationService.modifyPromotionAgreementStatus(updateStory));
@@ -222,6 +221,52 @@ class ProjectApplicationServiceTest {
         //then
         assertDoesNotThrow(() ->projectApplicationService.modifyReward(reward, shippingInfo));
 
+    }
+
+    @Test
+    @DisplayName("프로젝트 번호로 정책 정보 데이터 조회하는 테스트")
+    public void findPolicyByProjectNo_test() {
+
+        //given
+        int projectNo = 139;
+
+        //when
+        RefundPolicyDTO policy = projectApplicationService.findPolicyByProjectNo(projectNo);
+
+        //then
+        assertNotNull(policy);
+
+    }
+
+    @Test
+    @DisplayName("스토리 페이지에서 사용자가 입력한 값으로 기본데이터를 수정하는 테스트")
+    public void modifyPolicy_test() {
+
+        //given
+        int projectNo = 139;
+
+        //when
+        RefundPolicyDTO policy = projectApplicationService.findPolicyByProjectNo(projectNo);
+        policy.setRefundPolicyContent("서비스 수정 테스트");
+
+        //then
+        assertDoesNotThrow(() ->projectApplicationService.modifyPolicy(policy));
+
+    }
+
+    @Test
+    @DisplayName("반환정책 작성 페이지에서 펀딩금 반환 정책 정보 동의 상태 변경 테스트")
+    public void modifyPolicyAgreementStatus_test() {
+
+        //given
+        int projectNo = 139;
+
+        //when
+        RefundPolicyDTO policy = projectApplicationService.findPolicyByProjectNo(projectNo);
+        policy.setRefundPolicyAgreementStatus("Y");
+
+        //then
+        assertDoesNotThrow(() -> projectApplicationService.modifyPolicyAgreementStatus(policy));
     }
 
 }
