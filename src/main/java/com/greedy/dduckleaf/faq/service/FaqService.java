e63bedd184b31696bba2682fb04317b34663f299
yongseon_SC <shadow.zin73@gmail.com>
2022-04-29 16:25:58 +0900

    [FEAT] FAQ 상세 조회
    FAQ 상세 조회 기능을 구현하였습니다.

--- src/main/java/com/greedy/dduckleaf/faq/service/FaqService.java
+++ src/main/java/com/greedy/dduckleaf/faq/service/FaqService.java
@@ -14,6 +14,19 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * <pre>
+ * Class: FaqServiceTest
+ * Comment : 업무명
+ * History
+ * 2022/04/27 (이용선) 처음 작성 / FAQ 목록 조회 메소드 작성 시작
+ * 2022/04/28 (이용선) FAQ 상세조회 메소드 작성
+ * </pre>
+ * @version 1.0.1
+ *
+ * @author 이용선
+ */
+
 @Service
 public class FaqService {
 
@@ -27,7 +40,13 @@ public class FaqService {
         this.modelMapper = modelMapper;
     }
 
-
+    /**
+    * findFaqList : FAQ 목록을 조회합니다.
+    * @param pageable : 페이징 정보를 받는 객체입니다.
+    * @return faqDTO : faq 목록 정보를 반환합니다.
+     *
+    * @author 이용선
+    */
     public Page<FaqDTO> findFaqList(Pageable pageable){
 
         pageable = PageRequest.of(pageable.getPageNumber() <= 0? 0: pageable.getPageNumber() -1,
@@ -45,6 +64,21 @@ public class FaqService {
 
         return fq;
 
+    }
+
+    /**
+    * findFaqDetail : FAQ 상세정보를 조회합니다.
+    * @param faqNo : FAQ 번호를 전달받습니다.
+    * @return FaqDTO : 자주묻는 질문들의 정보를 반환합니다.
+     *
+    * @author 이용선
+    */
+
+    public FaqDTO findFaqDetail(int faqNo) {
+
+        Faq faq = faqRepository.findById(faqNo).get();
+
+        return modelMapper.map(faq, FaqDTO.class);
     }
 
 
