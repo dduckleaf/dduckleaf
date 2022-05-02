db08770c03c05ca94ef577108d4b39b747d33ce7
DESKTOP-RR7C6JL\Administrator <tkdjawlwhs@gmail.com>
2022-05-02 16:34:29 +0900

    [FEAT] 프로필 이메일 주소 변경 기능 추가

--- src/main/java/com/greedy/dduckleaf/profile/controller/ProfileController.java
+++ src/main/java/com/greedy/dduckleaf/profile/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package com.greedy.dduckleaf.profile.controller;
 
 import com.greedy.dduckleaf.authentication.model.dto.CustomUser;
+import com.greedy.dduckleaf.profile.dto.MemberDTO;
 import com.greedy.dduckleaf.profile.dto.ProfileAttachmentDTO;
 import com.greedy.dduckleaf.profile.service.ProfileService;
 import com.greedy.dduckleaf.projectnotice.dto.ProfileDTO;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,8 +30,9 @@ import java.util.UUID;
  * 2022/04/29 (박상범) 처음 작성 / 개인 정보 수정 페이지로 이동, 조회 관련 메소드 작성
  * 2022/04/30 (박상범) 회원의 사진 정보 변경 관련 메소드 작성
  * 2022/05/01 (박상범) 개인 정보 수정 페이지로 이동, 조회 관련 메소드 수정, 회원의 사진 정보 변경 관련 메소드 수정, 이메일 변경 페이지로 이동, 휴대전화 번호 변경 페이지로 이동, 비밀번호 변경 페이지로 이동 관련 메소드 수정
+ * 2022/05/02 (박상범) 이메일 인증번호 전송 관련 메소드 작성
  * </pre>
- * @version 1.0.5
+ * @version 1.0.6
  * @author 박상범
  */
 @Controller
@@ -155,5 +158,40 @@ public class ProfileController {
         }
 
         return result;
+    }
+
+    /**
+     * sendVerification: 입력받은 이메일 주소로 인증번호를 전송한다.
+     * @param email: 인증 번호를 받을 이메일 주소
+     * @return mv
+     * @author 박상범
+     */
+    @PostMapping("/send/email/verification")
+    @ResponseBody
+    public String sendVerification(String email) {
+
+        return profileService.sendEmailVerification(email);
+    }
+
+    /**
+     * modifyEmail: 회원의 이메일 주소를 변경합니다.
+     * @param email: 변경할 이메일 주소
+     * @param user: 로그인된 회원 정보
+     * @return mv
+     * @author 박상범
+     */
+    @PostMapping("/modify/email")
+    public ModelAndView modifyEmail(ModelAndView mv, String email, RedirectAttributes rttr, @AuthenticationPrincipal CustomUser user) {
+
+        MemberDTO member = new MemberDTO();
+        member.setMemberNo(user.getMemberNo());
+        member.setEmail(email);
+
+        profileService.modifyEmail(member);
+
+        rttr.addFlashAttribute("message", "이메일 주소가 변경되었습니다.");
+        mv.setViewName("/profile/modify/email");
+
+        return mv;
     }
 }
