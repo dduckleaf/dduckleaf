04acbca57ca78fb24449a1782c573f8fe4d5aced
DESKTOP-RR7C6JL\Administrator <tkdjawlwhs@gmail.com>
2022-05-09 02:06:45 +0900

    [FEAT] 관심 프로젝트 취소 뷰페이지와 컨트롤러 연동

--- src/main/java/com/greedy/dduckleaf/followingproject/controller/FollowingProjectController.java
+++ src/main/java/com/greedy/dduckleaf/followingproject/controller/FollowingProjectController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
  * Class: FollowingProjectController
  * Comment : 관심 프로젝트
  * History
- * 2022/05/08 (박상범) 처음 작성 / 관심 프로젝트 등록 관련 메소드 작성
+ * 2022/05/08 (박상범) 처음 작성 / 관심 프로젝트 등록 관련 메소드 작성, 관심 프로젝트 취소 관련 메소드 작성
  * </pre>
  * @version 1.0.1
  * @author 박상범
@@ -32,17 +32,17 @@ public class FollowingProjectController {
 
     /**
      * registFollowingProject: 관심 프로젝트로 등록합니다.
-     * @param data:  프로젝트 번호
+     * @param projectNo:  프로젝트 번호
      * @param user:  로그인된 회원의 정보
      * @return gson.toJson(result): "관심 프로젝트로 등록되었습니다."를 리턴합니다.
      * @author 박상범
      */
     @PostMapping(value = {"/regist"}, produces = "application/json; charset=UTF-8")
     @ResponseBody
-    public String registFollowingProject(@RequestBody int data, @AuthenticationPrincipal CustomUser user) {
+    public String registFollowingProject(@RequestBody int projectNo, @AuthenticationPrincipal CustomUser user) {
 
         FollowingProjectDTO followingProject = new FollowingProjectDTO();
-        followingProject.setProjectNo(data);
+        followingProject.setProjectNo(projectNo);
         followingProject.setMemberNo(user.getMemberNo());
 
         String result = followingProjectService.registFollowingProject(followingProject);
@@ -60,16 +60,16 @@ public class FollowingProjectController {
 
     /**
      * removeFollowingProject: 관심 프로젝트에서 제외합니다.
-     * @param data:  프로젝트 번호
+     * @param projectNo:  프로젝트 번호
      * @param user:  로그인된 회원의 정보
      * @return gson.toJson(result): "관심 프로젝트에서 제외되었습니다."를 리턴합니다.
      * @author 박상범
      */
     @PostMapping(value = {"/remove"}, produces = "application/json; charset=UTF-8")
     @ResponseBody
-    public String removeFollowingProject(@RequestBody int data, @AuthenticationPrincipal CustomUser user) {
+    public String removeFollowingProject(@RequestBody int projectNo, @AuthenticationPrincipal CustomUser user) {
 
-        String result = followingProjectService.removeFollowingProject(data, user.getMemberNo());
+        String result = followingProjectService.removeFollowingProject(projectNo, user.getMemberNo());
 
         Gson gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd")
