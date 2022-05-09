75738c7927e66e532f4915771a660a5ca9026e5e
Minjoo Jang <miinjoojang@gmail.com>
2022-05-09 18:27:20 +0900

    [FEAT] 회원 프로젝트 정산정보 조회 기능 추가
    HTML 파일에 데이터 연동 작업을 하였습니다.

--- src/main/java/com/greedy/dduckleaf/settlement/controller/SettlementController.java
+++ src/main/java/com/greedy/dduckleaf/settlement/controller/SettlementController.java
@@ -23,11 +23,11 @@ import org.springframework.web.servlet.ModelAndView;
  * 2022-04-30 (장민주) 처음 작성
  * 2022-05-01 (장민주) farmerSettlementOverviewPage 메소드 작성
  * 2022-05-06 (장민주) projectManageOverviewPage 메소드 작성
- *
+ * 2022-05-09 (장민주) farmerSettlementOverviewPage -> findFarmerSettlementOverview 메소드 명 변경 및 코드 수정
  * </pre>
  *
  * @author 장민주
- * @version 1.0.0
+ * @version 1.0.3
  */
 @Controller
 @RequestMapping("/settlement")
@@ -46,11 +46,28 @@ public class SettlementController {
     }
 
     /**
-     * farmerSettlementOverviewPage: 파머페이지 화면 경로를 찾아가기 위한 메소드입니다.
+     * findFarmerSettlementOverview: 파머페이지 화면 경로를 찾아가기 위한 메소드입니다.
+     * @param projectNo: 프로젝트번호
+     * @return mv: 프로젝트정보, 정산금관리 화면 경로
      * @author 장민주
      */
-    @GetMapping("/farmer/overview")
-    public void farmerSettlementOverviewPage() {}
+    @GetMapping("/farmer/overview/{projectNo}")
+    public ModelAndView findFarmerSettlementOverview(ModelAndView mv, @PathVariable int projectNo) {
+
+        SettlementInfoPackage settlementInfoPackage = calculateService.findSettlementInfoPackage(projectNo);
+
+        mv.addObject("project", settlementInfoPackage.getSettlementInfo().getProject());
+        System.out.println(settlementInfoPackage.getSettlementInfo().getProject());
+        mv.addObject("settlementInfo", settlementInfoPackage.getSettlementInfo());
+        System.out.println(settlementInfoPackage.getSettlementInfo());
+        mv.addObject("feeInfo", settlementInfoPackage.getFeeInfo());
+        System.out.println(settlementInfoPackage.getFeeInfo());
+        mv.addObject("settlementPaymentInfos", settlementInfoPackage.getSettlementPaymentInfos());
+        System.out.println(settlementInfoPackage.getSettlementPaymentInfos());
+        mv.setViewName("settlement/farmer/overview");
+
+        return mv;
+    }
 
     /**
      * projectManageOverviewPage: 프로젝트관리페이지 화면 경로를 찾아가기 위한 메소드입니다.
@@ -120,4 +137,5 @@ public class SettlementController {
 
         return mapper.writeValueAsString(settlementInfoPackage);
     }
+
 }
