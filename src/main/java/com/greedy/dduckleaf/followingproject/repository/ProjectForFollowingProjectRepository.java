eeb3b8004718846159ccd3ce6e786a6a9bf761f2
DESKTOP-RR7C6JL\Administrator <tkdjawlwhs@gmail.com>
2022-05-13 15:14:41 +0900

    [FEAT] 관심 프로젝트 프로젝트 사진 조회 추가

--- src/main/java/com/greedy/dduckleaf/followingproject/repository/ProjectForFollowingProjectRepository.java
+++ src/main/java/com/greedy/dduckleaf/followingproject/repository/ProjectForFollowingProjectRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
  * Class: ProjectForFollowingProjectRepository
  * Comment : 관심 프로젝트
  * History
- * 2022/05/0 (박상범) 처음 작성 / 관심 프로젝트 목록 조회 관련 메소드 작성
+ * 2022/05/08 (박상범) 처음 작성 / 관심 프로젝트 목록 조회 관련 메소드 작성
  * </pre>
  * @version 1.0.1
  * @author 박상범
