b7dce29b2eda472a5c9434b92ba047fa14e04b5a
LAPTOP-CBFQ98L2\hwiri <hwirim7907@gmail.com>
2022-05-13 11:43:34 +0900

    [FEAT] 프로젝트 관리에서 진행 중 프로젝트 목록 조회하는 기능 구현

--- src/main/java/com/greedy/dduckleaf/project/controller/ProjectManageController.java
+++ src/main/java/com/greedy/dduckleaf/project/controller/ProjectManageController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import static com.greedy.dduckleaf.project.finalfield.CodeForProject.BUTTON_AMOUNT;
 
@@ -85,13 +86,67 @@ public class ProjectManageController {
      * @author 박휘림
      */
     @GetMapping("/open")
-    public ModelAndView openProject(ModelAndView mv) {
+    public ModelAndView openProject(ModelAndView mv, RedirectAttributes rttr) {
 
-        projectManageService.openProject();
+        String result = projectManageService.openProject();
 
         mv.setViewName("redirect:/project/manage/scheduled/list");
+        rttr.addFlashAttribute("message", result);
+        return mv;
+    }
+
+    /**
+     * findProgressingProjectList: 진행 중인 프로젝트 목록을 조회하는 메소드입니다.
+     * @return 진행 중 프로젝트 목록, 프로젝트관리 진행 중 프로젝트 목록조회 화면경로
+     * @author 박휘림
+     */
+    @GetMapping("/progressing/list")
+    public ModelAndView findProgressingProjectList(ModelAndView mv, @PageableDefault Pageable pageable) {
+
+        Page<ProjectDTO> projectList = projectManageService.findProgressingProjectList(pageable);
+
+        PagingButtonInfo pagingInfo = Pagenation.getPagingButtonInfo(projectList, BUTTON_AMOUNT);
+
+        mv.addObject("pagingInfo", pagingInfo);
+        mv.addObject("projectList", projectList);
+        mv.addObject("intent", "progressing/list");
+        mv.setViewName("project/manage/progressing/list");
+        return mv;
+    }
+
+    /**
+     * findProgressingProjectApplicationInfoDetail: 진행 중 프로젝트 상세조회합니다.
+     * @param projectNo: 프로젝트 번호
+     * @return mv 뷰로 전달할 데이터와 경로를 담는 객체
+     *            detail 프로젝트 신청 내역 상세 조회 정보
+     *            "project/manage/progressing/detail" 진행 중 프로젝트 상세 페이지
+     * @author 박휘림
+     */
+    @GetMapping("/progressing/detail/{projectNo}")
+    public ModelAndView findProgressingProjectApplicationInfoDetail(ModelAndView mv, @PathVariable int projectNo) {
+
+        ProjectApplicationInfoDTO detail = projectManageService.findProjectApplicationDetail(projectNo);
+
+        mv.addObject("detail", detail);
+        mv.setViewName("project/manage/progressing/detail");
 
         return mv;
     }
 
+    /**
+     * openProject: 오픈예정 프로젝트를 진행 중으로 상태변경합니다.
+     * @return mv 뷰로 전달할 데이터와 경로를 담는 객체
+     *            "redirect:/project/manage/scheduled/list" 오픈예정 프로젝트 목록 조회
+     * @author 박휘림
+     */
+    @GetMapping("/end")
+    public ModelAndView endProject(ModelAndView mv, RedirectAttributes rttr) {
+
+        String result = projectManageService.endProject();
+
+        mv.setViewName("redirect:/project/manage/progressing/list");
+        rttr.addFlashAttribute("message", result);
+
+        return mv;
+    }
 }
